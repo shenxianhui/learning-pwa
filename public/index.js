f5769254f58e777fc8398871216975a853a9c5c2
shenxh <xianhui.shen@eslink.com>
2022-12-08 14:59:07 +0800

    feat: 上传项目

--- public/index.js
+++ public/index.js
@@ -1,161 +1,21 @@
-if ('serviceWorker' in navigator && 'PushManager' in window) {
-  var publicKey =
-    'BOEQSjdhorIf8M0XFNlwohK3sTzO9iJwvbYU-fuXRF0tvRpPPMGO6d_gJC_pUQwBT7wD8rKutpNTFHOHN3VqJ0A'
-  // 注册service worker
-  navigator.serviceWorker
-    .register('./sw.js')
-    .then(function (registration) {
-      console.log('Service Worker 注册成功')
-      // 开启该客户端的消息推送订阅功能
-      return subscribeUserToPush(registration, publicKey)
-    })
-    .then(function (subscription) {
-      // 将生成的客户端订阅信息存储在自己的服务器上
-      return sendSubscriptionToServer(JSON.stringify(subscription))
-    })
-    .then(function (res) {
-      console.log(res)
-    })
-    .catch(function (err) {
-      console.log(err)
-    })
-}
-
 function onClick(e) {
   getData()
 }
 
 function getData() {
   const contentDom = document.getElementById('content')
+  var xhr = new XMLHttpRequest()
   var url = 'https://api.wrdan.com/hitokoto'
 
   contentDom.innerHTML = ''
 
-  loading(true)
-  var remotePromise = getApiDataRemote(url)
-  getApiDataFromCache(url)
-    .then(function (data) {
-      if (data) {
-        loading(false)
-      }
-      cacheData = data || {}
-      return remotePromise
-    })
-    .then(function (data) {
-      if (JSON.stringify(data) !== JSON.stringify(cacheData)) {
-        contentDom.innerHTML = data.text
-        loading(false)
-      }
-    })
-}
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const res = JSON.parse(xhr.responseText)
 
-/**
- * 控制loading动画的展示
- * @param {boolean | undefined} isloading 是否展示loading
- */
-function loading(isloading) {
-  if (isloading) {
-    document.querySelector('#js-loading').style = 'display: block'
-  } else {
-    document.querySelector('#js-loading').style = 'display: none'
-  }
-}
-
-function getApiDataRemote(url) {
-  return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest()
-    xhr.timeout = 60000
-    xhr.onreadystatechange = function () {
-      var response = {}
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        try {
-          response = JSON.parse(xhr.responseText)
-        } catch (e) {
-          response = xhr.responseText
-        }
-        resolve(response)
-      } else if (xhr.readyState === 4) {
-        resolve()
-      }
+      contentDom.innerHTML = res.text
     }
-    xhr.onabort = reject
-    xhr.onerror = reject
-    xhr.ontimeout = reject
-    xhr.open('GET', url, true)
-    xhr.send(null)
-  })
-}
-/**
- * 获取该请求的缓存数据
- * @param {string} url 请求的url
- * @return {Promise}
- */
-function getApiDataFromCache(url) {
-  if ('caches' in window) {
-    return caches.match(url).then(function (cache) {
-      if (!cache) {
-        return
-      }
-      return cache.json()
-    })
-  } else {
-    return Promise.resolve()
   }
-}
-
-/**
- * 用户订阅相关的push信息
- * 会生成对应的pushSubscription数据，用于标识用户与安全验证
- * @param {ServiceWorker Registration} registration
- * @param {string} publicKey 公钥
- * @return {Promise}
- */
-function subscribeUserToPush(registration, publicKey) {
-  var subscribeOptions = {
-    userVisibleOnly: true,
-    applicationServerKey: window.urlBase64ToUint8Array(publicKey),
-  }
-  return registration.pushManager
-    .subscribe(subscribeOptions)
-    .then(function (pushSubscription) {
-      console.log(
-        'Received PushSubscription: ',
-        JSON.stringify(pushSubscription),
-      )
-      return pushSubscription
-    })
-}
-
-/**
- * 将浏览器生成的subscription信息提交到服务端
- * 服务端保存该信息用于向特定的客户端用户推送
- * @param {string} body 请求体
- * @param {string} url 提交的api路径，默认为/subscription
- * @return {Promise}
- */
-function sendSubscriptionToServer(body, url) {
-  url = url || '/subscription'
-  return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest()
-    xhr.timeout = 60000
-    xhr.onreadystatechange = function () {
-      var response = {}
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        try {
-          response = JSON.parse(xhr.responseText)
-        } catch (e) {
-          response = xhr.responseText
-        }
-        resolve(response)
-      } else if (xhr.readyState === 4) {
-        resolve()
-      }
-    }
-    xhr.onabort = reject
-    xhr.onerror = reject
-    xhr.ontimeout = reject
-    xhr.open('POST', url, true)
-    xhr.setRequestHeader('Content-Type', 'application/json')
-    xhr.send(body)
-  })
+  xhr.open('GET', url, true)
+  xhr.send(null)
 }
